1dc83035f45f7ab11a1f7bd9a01af94316671282
Yellamandarao Vemula <yalamanda118@gmail.com>
2023-12-18 12:40:36 +0530

    removed accounts module

--- src/app/modules/accounts/finance-accounts/finance-accounts.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FinanceAccountsRoutingModule } from './finance-accounts-routing.module';
-
-@NgModule({
-    declarations: [],
-    imports: [CommonModule, FinanceAccountsRoutingModule],
-})
-export class FinanceAccountsModule {}
--- src/app/modules/accounts/models/account-statement-query.model.ts
+++
@@ -1,4 +0,0 @@
-export class AccountStatementQuery {
-    public offset = 0;
-    public pageSize = 10;
-}
