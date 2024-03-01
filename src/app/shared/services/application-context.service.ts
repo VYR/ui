a1234fca38d7c0543d9e4797cb89a01f104cbb0c
Yellamandarao Vemula <yalamanda118@gmail.com>
2024-03-01 17:34:23 +0530

    user type changed

--- src/app/shared/services/application-context.service.ts
+++ src/app/shared/services/application-context.service.ts
@@ -61,7 +61,7 @@ export class ApplicationContextService {
         user.lastSignIn = data?.lastSignIn;
         user.entitlement = ( data?.user?.entitlement)?data?.user?.entitlement.split(',') || []:[];
         user.forcePasswordChange = data?.forcePasswordChange;
-        user.userType = data?.user?.userType;
+        user.userType = parseInt(data?.user?.userType);
         user.role = data?.user?.role || null;
         return user;
     }
